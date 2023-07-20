8bf3012b5fbbfbb32fc0462697253eb0279f0dcb
Max Schloegel <maxschloegel@gmx.net>
2023-07-21 00:08:48 +0200

    fix: drop out-of-border elements

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,10 @@ def go(args):
     # Convert format of date from string to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save cleaned dataframe
     df.to_csv("clean_sample.csv", index=False)
 
